aef163d0e5adb6ae689ca6b02ec8fb6892b0b33d
Maxine Tamas <mtamas25@cmc.edu>
2023-02-21 11:40:18 -0800

    binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -32,7 +32,7 @@ def find_smallest_positive(xs):
     elif xs[0] > 0:
         return 0
     elif xs[-1] > 0:
-        elif xs[-2] <= 0:
+        if xs[-2] <= 0:
             return len(xs) - 1
         else:
             return find_smallest_positive(xs[:-1])
